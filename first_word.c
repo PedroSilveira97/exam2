cbc911b2a2afc6ce836fe866a80d5694982c2123
Pedro Silveira <pedrotsilveira@gmail.com>
2025-11-12 14:07:39 +0000

    first_word.c

--- first_word.c
+++ first_word.c
@@ -6,20 +6,22 @@
 /*   By: ptavares <ptavares@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 13:33:30 by ptavares          #+#    #+#             */
-/*   Updated: 2025/11/12 13:36:53 by ptavares         ###   ########.fr       */
+/*   Updated: 2025/11/12 14:06:56 by ptavares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	first_word(char *str)
+void	ft_putchar(char c)
 {
-	if (!str)
-		return ;
-	while (*str != ' ' || *str != '	' || *str)
+	write(1, &c, 1);
+}
+
+void first_word(char *str)
+{
+	while (*str && *str != ' ' && *str != '	')
 	{
-		write(1,&str,1);
-		str++;
+		ft_putchar(*str++);
 	}
 }
 
@@ -27,5 +29,6 @@ int	main(int ac, char **av)
 {
 	if (ac == 2)
 		first_word(av[1]);
-	return (1);
+	write(1, "\n", 1);
+	return (0);
 }
